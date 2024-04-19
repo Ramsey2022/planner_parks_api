bf6491df2b942eefad0035cdfeb082f8eacc188d
Ramsey2022 <abovethelotus@yahoo.com>
2024-04-19 15:06:20 -0700

    added functionality to take in zipcodes and return nearby parks

--- app.py
+++ app.py
@@ -2,13 +2,25 @@ from flask import Flask, request, abort, Response, jsonify
 from dotenv import load_dotenv
 import os
 import googlemaps
-import pprint
-import time
 
 load_dotenv(".env")
 API_KEY = os.getenv("google_map_key")
 
 app = Flask(__name__)
+
+# Define client
+gmaps = googlemaps.Client(key=API_KEY)
+
+# Geocode a zipcode
+geocode_result = gmaps.geocode("97124")
+lat = geocode_result[0]["geometry"]["location"]["lat"]
+lon = geocode_result[0]["geometry"]["location"]["lng"]
+geocode_loc = (lat, lon)
+
+# Define search for parks
+parks_result = gmaps.places_nearby(
+    location=geocode_loc, radius=20000, open_now=False, type="park"
+)
 
 
 @app.route("/")
@@ -19,6 +31,11 @@ def root():
 @app.route("/health")
 def health():
     return jsonify(dict(status="OK")), 200
+
+
+@app.route("/parks")
+def parks():
+    return jsonify(parks_result)
 
 
 if __name__ == "__main__":
